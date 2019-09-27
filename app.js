2803b94f45843701fcf47538ffbc89eeb2519634
Anne Palangi <anne.palangi@gmail.com>
2019-09-26 20:59:15 -0400

    update url

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var randomArray = [0,1,2,3,4,5,6,7,8];
+//var randomArray = [0,1,2,3,4,5,6,7,8];
 
-for (var i = 0; i < randomArray.length; i++){
-console.log(i);
+//for (var i = 0; i < randomArray.length; i++){
+//console.log(i);
     $.ajax({
-        url: 'https://xkcd.com/' + randomArray[i] + '/info.0.json',
+        url: 'http://xkcd.com/info.0.json',
+//        url: 'https://xkcd.com/' + randomArray[i] + '/info.0.json',
         //url: 'https://api.apis.guru/v2/specs/xkcd.com/1.0.0/swagger.json',
         method: "GET"
     }).done(function (response) {
